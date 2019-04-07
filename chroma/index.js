a09c7607b3cb8d5f93b0185ecd4b2f44843059d1
Evangelina Ferreira <evaferreira92@hotmail.com>
2019-04-07 18:21:27 -0300

    Fix Firefox + color picker

--- chroma/index.js
+++ chroma/index.js
@@ -1,42 +1,84 @@
-var compute_frame = function() {
+
+var color;
+var player = document.getElementById('video');
+
+var handleSuccess = function(stream) {
+	// Attach the video stream to the video element and autoplay.
+	player.srcObject = stream;
+	loadAll();
+};
+
+// Get video
+navigator.mediaDevices.getUserMedia({video: true})
+  .then(handleSuccess);
+
+
+function pickedFilter () {
+	var selector = document.getElementById("colorFilter");
+	color = selector.options[selector.selectedIndex].value;
+	loadAll();
+}
+
+pickedFilter();
+
+colorFilter.addEventListener("change", pickedFilter);
+
+function compute_frame(color) {
     this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
-    let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
+    let frame = this.ctx1.getImageData(0, 0, 640, 480);
 	
-	// 640x480 x 4 bytes per pixel = 1228800 = l
+	// 640x480 x 4 because rgba per pixel = 1228800 = l
     let l = frame.data.length / 4;
+	
+	if (color === "Blue") {
+		for (let i = 0; i < l; i++) {
+			
+			let r = frame.data[i * 4 + 0];
+			let g = frame.data[i * 4 + 1];
+			let b = frame.data[i * 4 + 2];
 
-    for (let i = 0; i < l; i++) {
-        
-        let r = frame.data[i * 4 + 0];
-        let g = frame.data[i * 4 + 1];
-        let b = frame.data[i * 4 + 2];
+			if (b > r && b > g)
+			{
+				if (b > 100 ) {
+					frame.data[i * 4 + 3] = 0;
+				}
+			}
+				
+		}
+	} else {
+		for (let i = 0; i < l; i++) {
+			
+			let r = frame.data[i * 4 + 0];
+			let g = frame.data[i * 4 + 1];
+			let b = frame.data[i * 4 + 2];
 
-        if (g > r && g > b)
-        {
-            if (g > 100 ) {
-                frame.data[i * 4 + 3] = 0;
-            }
-        }
-            
-    }
+			if (g > r && g > b)
+			{
+				if (g > 100 ) {
+					frame.data[i * 4 + 3] = 0;
+				}
+			}
+				
+		}
+	}
     this.ctx2.putImageData(frame, 0, 0);
     return;
 };
 
-var time_callback = function() {
+function time_callback () {
     if (this.video.paused || this.video.ended) {
       return;
     }
-    this.computeFrame();
+    compute_frame(color);
     let self = this;
 	
 	// Update
     setTimeout(function () {
-        self.timerCallback();
-      }, 0);
+		self.time_callback();
+    }, 0);
 };
 
-var do_load = function() {
+function loadAll () {
     this.video = document.getElementById("video");
     this.c1 = document.getElementById("c1");
     this.ctx1 = this.c1.getContext("2d");
@@ -46,12 +88,6 @@ var do_load = function() {
     this.video.addEventListener("play", function() {
         self.width = self.video.videoWidth;
         self.height = self.video.videoHeight;
-        self.timerCallback();
-      }, false);
-};
-
-var processor = {
-    doLoad: do_load,
-    timerCallback: time_callback, 
-    computeFrame: compute_frame 
+        self.time_callback();
+    }, false);
 };
